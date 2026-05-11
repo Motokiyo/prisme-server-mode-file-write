68789edf5b08f19cf5cd4aba36574db891a99c09
Thierry Templier <templth@gmail.com>
2026-05-11 15:55:07 +0200

    fix(build): skip native rebuild and rely on prebuilt platform binaries

--- packages/desktop/electron-builder.config.ts
+++ packages/desktop/electron-builder.config.ts
@@ -38,6 +38,7 @@ const githubPublish = {
 const getBase = (): Configuration => ({
   artifactName: "prisme-desktop-${os}-${arch}.${ext}",
   publish: [githubPublish],
+  npmRebuild: false,
   directories: {
     output: "dist",
     buildResources: "resources",
